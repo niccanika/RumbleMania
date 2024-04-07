bb5276d666f7b87c8112f6677739b269faa70ea2
Nikola Janíčková <njanickova8@gmail.com>
2024-04-07 20:59:03 +0200

    implemented side to side movement and punch attack

--- Assets/Scripts/Player1Move.cs
+++ Assets/Scripts/Player1Move.cs
@@ -6,24 +6,51 @@ public class Player1Move : MonoBehaviour
 {
 
     private Animator Anim;
+    public float WalkSpeed = 0.001f;
+    private bool canWalkLeft = true;
+    private bool canWalkRight = true;
+    public GameObject Player1;
 
     // Start is called before the first frame update
     void Start()
     {
-        Anim = GetComponent<Animator>();
+        Anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 ScreenBounds = Camera.main.WorldToScreenPoint(this.transform.position);
+
+        // screen bounds
+        if(ScreenBounds.x > Screen.width - 200)
+        {
+            canWalkRight = false;
+        }
+
+        if(ScreenBounds.x < 200)
+        {
+            canWalkLeft = false;
+        } else if (ScreenBounds.x > 200 && ScreenBounds.x < Screen.width - 200) {
+            canWalkRight = true;
+            canWalkLeft = true;
+        }
+
+        //walking left and right
         if(Input.GetAxis("Horizontal") > 0)
         {
-            Anim.SetBool("Forward", true);
+            if(canWalkRight == true){
+                Anim.SetBool("Forward", true);
+                transform.Translate(WalkSpeed, 0, 0);
+            }
         }
 
         if(Input.GetAxis("Horizontal") < 0)
         {
-            Anim.SetBool("Backward", true);
+            if(canWalkLeft == true){
+                Anim.SetBool("Backward", true);
+                transform.Translate(-WalkSpeed, 0, 0);
+            }
         }
 
         if(Input.GetAxis("Horizontal") == 0)
