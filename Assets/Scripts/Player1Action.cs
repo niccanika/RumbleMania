a6cff3345de104d2dbdb9bfd69ce6e1d1e08be6c
Nikola Janíčková <njanickova8@gmail.com>
2024-06-20 17:16:53 +0200

    improved leg sweep attack

--- Assets/Scripts/Player1Action.cs
+++ Assets/Scripts/Player1Action.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour
     public float JumpSpeed = 1.0f;
     public GameObject Player1;
     private Animator Anim;
+    private AnimatorStateInfo Player1Layer0;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,25 +20,43 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+
+        //listen to animator
+        Player1Layer0 = Anim.GetCurrentAnimatorStateInfo(0);
+
+        //standing attacks
+        if(Player1Layer0.IsTag("Motion"))
         {
-            Anim.SetTrigger("LightPunch");
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Anim.SetTrigger("LightPunch");
+            }
+
+            if (Input.GetButtonDown("Fire2"))
+            {
+                Anim.SetTrigger("HeavyPunch");
+            }
+
+            if (Input.GetButtonDown("Fire3"))
+            {
+                Anim.SetTrigger("LightKick");
+            }
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                Anim.SetTrigger("HeavyKick");
+            }
         }
 
-        if (Input.GetButtonDown("Fire2"))
+        //Crouching attack
+        if (Player1Layer0.IsTag("Crouching"))
         {
-            Anim.SetTrigger("HeavyPunch");
+            if (Input.GetButtonDown("Fire3"))
+            {
+                Anim.SetTrigger("LightKick");
+            }
         }
 
-        if (Input.GetButtonDown("Fire3"))
-        {
-            Anim.SetTrigger("LightKick");
-        }
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            Anim.SetTrigger("HeavyKick");
-        }
     }
 
     public void JumpUp(){
